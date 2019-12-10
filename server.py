a5cc290c50feb8ed63b153587f147da7fbf1f229
Alexander Grinev <grinev95@bk.ru>
2019-12-11 02:57:53 +0700

    Init commit

---
+++ server.py
@@ -0,0 +1,70 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import ServerFactory, connectionDone
+from twisted.protocols.basic import LineOnlyReceiver
+
+
+class ServerProtocol(LineOnlyReceiver):
+    factory: 'Server'
+    login: str = None
+
+    def lineReceived(self, line: bytes):
+        content = line.decode()
+
+        if self.login is not None:
+            content = f"Message from {self.login}: {content}"
+            self.factory.history.append(content)
+
+            for user in self.factory.clients:
+                if user is not self and user.login is not None:
+                    user.sendLine(content.encode())
+        else:
+            if content.startswith("login:"):
+                temp_login = content.replace("login:", "")
+
+                if temp_login != "" and self.is_login_free(temp_login):
+                    self.login = temp_login
+                    self.sendLine(f"Welcome, {self.login}!".encode())
+                    self.factory.users.append(temp_login)
+                    self.send_history()
+                else:
+                    self.sendLine(f"Login '{temp_login}' is invalid or taken. Try another one.".encode())
+            else:
+                self.sendLine("Unauthorized.".encode())
+
+    def connectionLost(self, reason=connectionDone):
+        self.factory.clients.remove(self)
+        self.factory.users.remove(self.login)
+
+    def connectionMade(self):
+        self.factory.clients.append(self)
+
+    def is_login_free(self, login):
+        for user in self.factory.users:
+            if login == user:
+                return False
+
+        return True
+
+    def send_history(self):
+        for message in self.factory.history[-10:]:
+            self.sendLine(message.encode())
+
+
+class Server(ServerFactory):
+    protocol = ServerProtocol
+    clients: list
+    users: list
+    history: list
+
+    def startFactory(self):
+        self.clients = []
+        self.users = []
+        self.history = []
+        print("Server started")
+
+    def stopFactory(self):
+        print("Server closed")
+
+
+reactor.listenTCP(1234, Server())
+reactor.run()
